a33f6458810601d09d9b0cad65d6f6d1b9ad69d3
madmax22869 <smaxim1970@gmail.com>
2024-04-24 21:58:21 +1200

    added likedsongs

---
+++ model/likedSong.js
@@ -0,0 +1,15 @@
+//Bring necessary libraries
+const mongoose = require('mongoose'); 
+
+// song storage
+
+const likedSongs = new mongoose.Schema({
+    name:{
+        required: true,
+        type: String
+    },
+    user:{
+        required: true,
+        type: String
+    }
+})
--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const Model = require('../model/model');
 const LoginModel = require('../model/loginModel');
+const likedSongs = require('../model/likedSong');
 const jwt = require("jsonwebtoken");
 
 //Initialise
 const router = express.Router();
 
-//Helpers
+//check token
 function authenticateToken(req, res, next) {
     const token = req.headers['authorization'];
     if (token == null) return res.sendStatus(401); // if there isn't any token
@@ -56,6 +57,21 @@ router.post('/post', authenticateToken, async (req,res)=>{
     }
 });
 
+// add new liked song
+
+router.post('/addNewSong', authenticateToken, async (req,res)=>{
+    const songData = new likedSongs({
+        name: req.body.name,
+        user: req.body.user
+    });
+    try {
+        const storeData = await songData.save();
+        res.status(200).json(storeData);
+    } catch{
+        res.status(400).json({message: error.message});
+    }
+})
+
 //Get all
 router.get('/getAll', authenticateToken, async (req,res)=>{
     try {
@@ -65,6 +81,17 @@ router.get('/getAll', authenticateToken, async (req,res)=>{
         res.status(500).json({message: error.message});
     }
 });
+
+// Get all songs
+
+router.get('/getAllSongs', authenticateToken, async (req,res)=>{
+    try {
+        const data = await likedSongs.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
 //Get by id
 router.get('/getById/:id', authenticateToken, async (req,res)=>{
@@ -100,11 +127,37 @@ try {
 }
 });
 
+//update song
+
+router.patch('/updateSongByName/:name', authenticateToken, async (req,res)=>{
+    try {
+        const name = req.params.name;
+        const updateData = req.body;
+        const options = {new: true};
+        const result = await likedSongs.findByIdAndUpdate(id, updateData, options);
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+    });
+
 //Delete by id
 router.delete('/deleteById/:id', authenticateToken, async (req,res)=>{
     try {
         const id = req.params.id;
         const data = await Model.findByIdAndDelete(id);
+        res.send(`Document with ${data.name} has been deleted.`);
+    } catch (error) {
+        res.status(400).json({message: error});
+    }
+    });
+
+// delete song by name
+
+router.delete('/deleteSongByName/:name', authenticateToken, async (req,res)=>{
+    try {
+        const name = req.params.name;
+        const data = await likedSongs.findByIdAndDelete(id);
         res.send(`Document with ${data.name} has been deleted.`);
     } catch (error) {
         res.status(400).json({message: error});
