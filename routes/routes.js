5350332b14ac9e904d175bc285058b80f59de8ef
madmax22869 <smaxim1970@gmail.com>
2024-04-25 12:44:00 +1200

    clean up 2.0

--- routes/routes.js
+++ routes/routes.js
@@ -23,6 +23,48 @@ function authenticateToken(req, res, next) {
 //API endpoints
 
 
+//Login
+router.post('/login', async (req,res)=>{
+    const userName = req.body.login;
+    const password = req.body.password;
+    try {
+        const data = await LoginModel.findOne({login:userName});
+        if(password == data.password){
+                const token = jwt.sign({username:userName},process.env.ACCESS_TOKEN_SECRET);
+                res.status(200).json({token:token});
+            }else{
+                res.status(401).json("Username or password incorrect.")
+            }
+            
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+//Get all users
+router.get('/getAllUsers', authenticateToken, async (req,res)=>{
+    try {
+        const data = await LoginModel.find();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+//Get user by name(user)
+router.get('/getUserByName/:user', authenticateToken, async (req,res)=>{
+    try {
+        const user = req.params.user;
+        const data = await LoginModel.findOne({user:user});
+        if(data == null)         
+            res.status(404).json({message: "Not found"});
+        else
+            res.json(data);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+})
+
 //add new user
 router.post('/addNewUser', authenticateToken, async (req,res)=>{
     const data = new LoginModel({
@@ -38,76 +80,33 @@ router.post('/addNewUser', authenticateToken, async (req,res)=>{
     }
 });
 
-//Login
-router.post('/login', async (req,res)=>{
-    const userName = req.body.login;
-    const password = req.body.password;
-    try {
-        const data = await LoginModel.findOne({login:userName,password:password});
-        if(password == data.password){
-                const token = jwt.sign({username:userName},process.env.ACCESS_TOKEN_SECRET);
-                res.status(200).json({token:token});
-            }else{
-                res.status(401).json("Username or password incorrect.")
-            }
-            
-    } catch (error) {
-        res.status(500).json({error: error});
-    }
-});
-
-//Get all users
-router.get('/getAll', authenticateToken, async (req,res)=>{
-    try {
-        const data = await LoginModel.find();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-//Get User by Login
-// router.get('/getUserByLogin/:login', authenticateToken, async (req,res)=>{
-//     try {
-//         const data = await LoginModel.findById(req.params.login);
-//         res.json(data);
-//     } catch (error) {
-//         res.status(404).json({message: error.message});
-//     }
-// });
-
-//Get user by name(user)
-router.get('/getByName/:user', authenticateToken, async (req,res)=>{
-    try {
-        const name = req.params.name;
-        const data = await LoginModel.findOne({user});
-        res.json(data);
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-})
-
 //Update User by id
 router.patch('/updateUserBylogin/:login', authenticateToken, async (req,res)=>{
 try {
-    const id = req.params.login;
+    const login = req.params.login;
     const updateData = req.body;
     const options = {new: true};
-    const result = await LoginModel.findByIdAndUpdate(id, updateData, options);
-    res.send(result);
+    const data = await LoginModel.findOneAndUpdate({login:login}, updateData, options);
+    if(data == null)         
+        res.status(404).json({message: "Not found"});
+    else
+        res.send(data);
 } catch (error) {
-    res.status(400).json({message: error});
+    res.status(400).json({message: error.message});
 }
 });
 
 //Delete user by id(login)
 router.delete('/deleteUserByLogin/:login', authenticateToken, async (req,res)=>{
     try {
-        const id = req.params.login;
-        const data = await LoginModel.findByIdAndDelete(login);
-        res.send(`User with ${data.login} has been deleted.`);
+        const login = req.params.login;
+        const data = await LoginModel.findOneAndDelete({login:login});
+        if(data == null)         
+            res.status(404).json({message: "Not found"});
+        else
+            res.send(`User with ${data.login} has been deleted.`);
     } catch (error) {
-        res.status(400).json({message: error});
+        res.status(400).json({message: error.message});
     }
     });
 
@@ -135,28 +134,48 @@ router.get('/getAllSongs', authenticateToken, async (req,res)=>{
     }
 })
 
-//update song
-router.patch('/updateSongByName/:name', authenticateToken, async (req,res)=>{
+//Get song by name
+router.get('/getSongsByName/:name', authenticateToken, async (req,res)=>{
     try {
         const name = req.params.name;
+        const data = await likedSongs.find({name:name});
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+    });
+
+
+//update song
+router.patch('/updateSongByName/:name/:user', authenticateToken, async (req,res)=>{
+    try {
+        const name = req.params.name;
+        const user = req.params.user;
         const updateData = req.body;
         const options = {new: true};
-        const result = await likedSongs.findByIdAndUpdate(id, updateData, options);
-        res.send(result);
+        const data = await likedSongs.findOneAndUpdate({name:name,user:user}, updateData, options);
+        if(data == null)         
+            res.status(404).json({message: "Not found"});
+        else
+            res.send(data);
     } catch (error) {
-        res.status(400).json({message: error});
+        res.status(400).json({message: error.message});
     }
     });
 
 // delete song by name
 
-router.delete('/deleteSongByName/:name', authenticateToken, async (req,res)=>{
+router.delete('/deleteSongByName/:name/:user', authenticateToken, async (req,res)=>{
     try {
         const name = req.params.name;
-        const data = await likedSongs.findByIdAndDelete(id);
-        res.send(`Document with ${data.name} has been deleted.`);
+        const user = req.params.user;
+        const data = await likedSongs.findOneAndDelete({name:name,user:user});
+        if(data == null)         
+            res.status(404).json({message: "Not found"});
+        else
+            res.send(`Document with ${data.name} for ${data.user} has been deleted.`);
     } catch (error) {
-        res.status(400).json({message: error});
+        res.status(400).json({message: error.message});
     }
     });
 
